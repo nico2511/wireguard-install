2a42f19d6f4ecaf1f4a4d60f58d73aa2ce060e0d
NiKo <62502164+nico2511@users.noreply.github.com>
2024-06-22 16:06:18 +0200

    Update wireguard-install.sh
    Add FDN DNS (French Data Network)
    
    https://www.fdn.fr/actions/dns/

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -105,8 +105,9 @@ new_client_dns () {
 	echo "   4) OpenDNS"
 	echo "   5) Quad9"
 	echo "   6) AdGuard"
+ 	echo "   7) French Data Network (FDN)
 	read -p "DNS server [1]: " dns
-	until [[ -z "$dns" || "$dns" =~ ^[1-6]$ ]]; do
+	until [[ -z "$dns" || "$dns" =~ ^[1-7]$ ]]; do
 		echo "$dns: invalid selection."
 		read -p "DNS server [1]: " dns
 	done
@@ -137,6 +138,9 @@ new_client_dns () {
 		;;
 		6)
 			dns="94.140.14.14, 94.140.15.15"
+		;;
+  		7)
+			dns="80.67.169.12,2001:910:800::12,80.67.169.40,2001:910:800::40"
 		;;
 	esac
 }
